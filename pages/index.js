57da800289731dbbe6792080a129b51007c474b5
Vivek Kumar <vivekkumarsahani169@gmail.com>
2025-09-29 13:39:10 +0530

    some chnages

--- pages/index.js
+++ pages/index.js
@@ -9,37 +9,32 @@ export default function Home() {
           <h1 className="text-4xl font-bold mb-4 text-blue-600">
             Welcome to my portfolio
           </h1>
-
           <p className="text-sm text-gray-500 mb-2">
             I&apos;m a developer passionate about creating amazing web
             experiences.
           </p>
-
           <p className="text-sm text-gray-500">
-            Explore the{" "}
-            <Link
-              href="/about"
-              className="text-orange-500 font-semibold hover:underline"
-            >
-              About
-            </Link>
-            ,{" "}
-            <Link
-              href="/contact"
-              className="text-orange-500 font-semibold hover:underline"
-            >
-              Contact
-            </Link>{" "}
-            and{" "}
-            <Link
-              href="/blog"
-              className="text-orange-500 font-semibold hover:underline"
-            >
-              Blog
-            </Link>{" "}
-            sections to learn more. With Tailwind CSS, the design is clean,
-            responsive, and user-friendly.
+            Explore the <strong>About</strong>, <strong>Contact</strong>, and{" "}
+            <strong>Blog</strong> sections to learn more. With Tailwind CSS, the
+            design is clean, responsive, and user-friendly.
           </p>
+          <div className="flex items-center justify-center gap-6  mt-16">
+            <Link
+              href="https://github.com/vivekkumarshani/my-nextjs-app"
+              target="_blank"
+              className="px-8 py-4 bg-orange-500 hover:bg-orange-600 transition-colors duration-300 text-white no-underline rounded-lg font-bold"
+            >
+              <button>GitHub Repo</button>
+            </Link>
+
+            <Link
+              href="https://my-first-nextjs-app-portfolio.netlify.app/"
+              target="_blank"
+              className="px-8 py-4 bg-green-500 hover:bg-green-600 transition-colors duration-300 text-white no-underline rounded-lg font-bold"
+            >
+              <button>Live Demo</button>
+            </Link>
+          </div>
         </div>
       </Layout>
     </div>
